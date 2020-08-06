476b85a04051aed0271c890fd59393166126de7b
tooomm <tooomm@users.noreply.github.com>
2020-08-06 16:28:17 +0200

    set crown in brackets

--- client/src/App.vue
+++ client/src/App.vue
@@ -738,11 +738,11 @@
 							<h2>Wait for {{ userByID[sessionOwner].userName }}</h2>
 						</div>
 						<div class="welcome-section">
-							{{ userByID[sessionOwner].userName }} is the session owner
+							{{ userByID[sessionOwner].userName }} is the session owner (
 							<i
 								class="fas fa-crown subtle-gold"
 							></i>
-							. Wait for them to select the options and launch a game!
+							). Wait for them to select the options and launch a game!
 							<br />You can still customize your personal options on top of the page.
 						</div>
 					</div>
